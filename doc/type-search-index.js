146cc25fc149b7d79ca51f4c3a24f7279d5f8a9e
stef <off.a@aol.com>
2024-03-12 19:32:04 +0100

    directory passed to parent directory to get complete java program

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"classes","l":"Boss"},{"p":"classes","l":"Methods"},{"p":"program","l":"Program"},{"p":"classes","l":"User"},{"p":"classes","l":"WalletMaths"}];updateSearchResults();
